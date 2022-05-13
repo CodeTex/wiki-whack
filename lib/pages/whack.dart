3940f42d0586cf08ae238f19eff1fc21c7331cf1
CodeTex <29424456+CodeTex@users.noreply.github.com>
2022-05-13 23:16:14 +0200

    ADD basic page layout

---
+++ lib/pages/whack.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WhackPage extends StatelessWidget {
+  const WhackPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
